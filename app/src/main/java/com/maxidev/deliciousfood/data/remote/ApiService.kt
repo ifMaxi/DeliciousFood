8c0059942e10bdd3be95028fdbca108f5b5718a3
Maxi Pelizzoni <maxi96p@gmail.com>
2024-08-05 19:17:51 -0300

    Removed unnecessary components.

--- app/src/main/java/com/maxidev/deliciousfood/data/remote/ApiService.kt
+++ app/src/main/java/com/maxidev/deliciousfood/data/remote/ApiService.kt
@@ -1,7 +1,11 @@
 package com.maxidev.deliciousfood.data.remote
 
+import com.maxidev.deliciousfood.data.remote.dto.CategoriesDTO
+import com.maxidev.deliciousfood.data.remote.dto.FilterByCategoryDTO
 import com.maxidev.deliciousfood.data.remote.dto.MealDTO
 import com.maxidev.deliciousfood.utils.Constants.BY_ID
+import com.maxidev.deliciousfood.utils.Constants.FILTER
+import com.maxidev.deliciousfood.utils.Constants.MEAL_CATEGORIES
 import com.maxidev.deliciousfood.utils.Constants.RANDOM_SINGLE
 import com.maxidev.deliciousfood.utils.Constants.SEARCH
 import retrofit2.http.GET
@@ -22,4 +26,14 @@ interface ApiService {
 
     @GET(RANDOM_SINGLE)
     suspend fun getRandomMeal(): MealDTO
+
+    @GET(MEAL_CATEGORIES)
+    suspend fun getCategories(
+        @Query("page") page: String?
+    ): CategoriesDTO
+
+    @GET(FILTER)
+    suspend fun getFilterByCategory(
+        @Query("c") c: String
+    ): FilterByCategoryDTO
 }
--- app/src/main/java/com/maxidev/deliciousfood/di/RepositoryModule.kt
+++ app/src/main/java/com/maxidev/deliciousfood/di/RepositoryModule.kt
@@ -1,11 +1,9 @@
 package com.maxidev.deliciousfood.di
 
-import com.maxidev.deliciousfood.data.repository.CategoriesRepositoryImpl
 import com.maxidev.deliciousfood.data.repository.DetailMealRepositoryImpl
 import com.maxidev.deliciousfood.data.repository.FavoriteRepositoryImpl
 import com.maxidev.deliciousfood.data.repository.FilterByRepositoryImpl
 import com.maxidev.deliciousfood.data.repository.HomeRepositoryImpl
-import com.maxidev.deliciousfood.domain.repository.CategoriesRepository
 import com.maxidev.deliciousfood.domain.repository.DetailMealRepository
 import com.maxidev.deliciousfood.domain.repository.FavoriteRepository
 import com.maxidev.deliciousfood.domain.repository.FilterByRepository
@@ -27,11 +25,6 @@ abstract class RepositoryModule {
 
     @Binds
     abstract fun bindHomeRepository(repository: HomeRepositoryImpl): HomeRepository
-
-    @Binds
-    abstract fun bindCategoriesRepository(
-        repository: CategoriesRepositoryImpl
-    ): CategoriesRepository
 
     @Binds
     abstract fun bindFilterByRepository(repository: FilterByRepositoryImpl): FilterByRepository
