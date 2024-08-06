cccd4e205e1d52bebdea84640d6a759e2a6b82e8
Maxi Pelizzoni <maxi96p@gmail.com>
2024-08-06 19:36:48 -0300

    Fonts added.

--- app/src/main/java/com/maxidev/deliciousfood/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/maxidev/deliciousfood/presentation/ui/theme/Type.kt
@@ -2,33 +2,33 @@ package com.maxidev.deliciousfood.presentation.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.maxidev.deliciousfood.R
+
+val openSans = FontFamily(
+    Font(R.font.open_sans_light, weight = FontWeight.Light),
+    Font(R.font.open_sans, weight = FontWeight.Normal),
+    Font(R.font.open_sans_medium, weight = FontWeight.Medium),
+    Font(R.font.open_sans_bold, weight = FontWeight.Bold),
+    Font(R.font.open_sans_semibold, weight = FontWeight.SemiBold),
+    Font(R.font.open_sans_extrabold, weight = FontWeight.ExtraBold)
+)
+
+val lobster = FontFamily(
+    Font(R.font.lobster_two, weight = FontWeight.Normal),
+    Font(R.font.lobster_two_bold, weight = FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = openSans,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
