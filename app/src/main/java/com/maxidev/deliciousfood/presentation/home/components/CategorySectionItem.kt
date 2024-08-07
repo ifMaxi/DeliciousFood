8ddf489432f764655d61e0a3eb65ed5d5f2e2009
Maxi Pelizzoni <maxi96p@gmail.com>
2024-08-07 19:19:57 -0300

    Colors and app icon added. Change from card to outlined card.

--- app/src/main/java/com/maxidev/deliciousfood/presentation/home/components/CategorySectionItem.kt
+++ app/src/main/java/com/maxidev/deliciousfood/presentation/home/components/CategorySectionItem.kt
@@ -7,9 +7,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -29,7 +29,7 @@ fun CategorySectionItem(
     strCategoryThumb: String,
     onClick: () -> Unit
 ) {
-    Card(
+    OutlinedCard(
         modifier = modifier
             .wrapContentHeight(Alignment.Top),
         onClick = onClick,
